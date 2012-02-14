064edaaeb6388e0514360ad1d673ba0c2f2f5112
Mikkel Kaas <mikkel@uniqma.dk>
2012-02-14 08:45:39 +0100

    Update 14.02.2012

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -3,8 +3,8 @@
   $dbhost = 'localhost';
   $dbbase = 'rcman';
   
-  $dbuser = 'root';
-  $dbpass = '';
+  $dbuser = 'webdev';
+  $dbpass = 'webdev';
   
   mysql_connect($dbhost,$dbuser,$dbpass) or die(mysql_error());
   mysql_select_db($dbbase) or die(mysql_error());
